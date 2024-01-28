7fcbc88fe85df512f4abd7e89481ede15d6d2f88
HELLOSJ00 <skku03062@g.skku.edu>
2024-01-28 23:47:15 +0900

    [style] 로그인 로직 수정

--- App/src/screens/Main/MyPage/index.tsx
+++ App/src/screens/Main/MyPage/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import styled from 'styled-components';
 
 const MyPage: React.FC = () => {
     return <Text>MyPage</Text>;
